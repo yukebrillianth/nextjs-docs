0348e98c30b8a03a01a6f37b4e772cab21e38d1c
Yuke Brilliant H <61053094+yukebrillianth@users.noreply.github.com>
2021-04-06 20:00:33 +0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,6 @@
 export default {
   repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – UpVote',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
@@ -15,15 +15,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the next site builder" />
-      <meta name="og:description" content="Nextra: the next site builder" />
+      <meta name="description" content="UpVote: Aplikasi Pemilihan Ketua Osis" />
+      <meta name="og:description" content="UpVote: Aplikasi Pemilihan Ketua Osis" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="og:title" content="UpVote: Aplikasi Pemilihan Ketua Osis" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="UpVote" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
       <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
